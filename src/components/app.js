effc54b02691db21b4e44c4b7e1c1e8c8d8d46b1
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-10-31 00:51:01 -0700

    Completed prototype functional-components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
+import React from 'react'
+import Table from './table';
+
+const students = [
+    {
+        name: 'Tony Ony',
+        course: 'Biology',
+        grade: 79
+    },
+    {
+        name: 'Bony Sony',
+        course: 'Diology',
+        grade: 89
+    },
+    {
+        name: 'Kony Dony',
+        course: 'Triology',
+        grade: 99
+    }
+];
+export default () =>{
+    return (<div className="container">
+        <h1>Student Grade Table</h1>
+        <Table data= {students}/>
+    </div>
+    )
+}
+
 
 
