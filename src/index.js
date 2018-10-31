d75e6582516676509a9fa270c9bd53fe21002d4b
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-10-30 23:05:17 -0700

    Completed react-intro prototype

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const userObject = {
+    'name': "Sasha ",
+    'luckyNumber': luckyNumber()
+};
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(userObject),
     document.getElementById('root')
-);
+)
+
+
+
+function luckyNumber() {
+    return Math.floor(Math.random() * 1000) +1;
+};
+console.log(userObject.luckyNumber);
+function greeting(user){
+    return <div className="container">
+    <h1 className="text-muted">Welcome {user.name} !</h1>
+    <h2 className="text-muted">Your lucky number is: {user.luckyNumber} !!!</h2>
+    </div>
+};
