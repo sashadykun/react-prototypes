844260610dd796574b85a7f258768366bd71f82d
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-11-04 20:32:38 -0800

    completed requirement except class translate to functin

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,42 @@
 
 
+import React, { Component } from 'react';
+import ContactList from './contact_list';
+import ContactForm from './contact_form';
+import contactData from '../data/contacts';
+
+class App extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            contacts: contactData
+        }
+        this.addContact=this.addContact.bind(this);
+    }
+
+    addContact(contact){
+        console.log('contact:', contact);
+        this.setState({
+            contacts: [...this.state.contacts, contact]
+        
+        })
+    }
+   
+
+    render(){
+        return (
+            <div className="container">
+                <h1 className="text-center my-3">Adress Book</h1>
+                <div className="row">
+                    <div className="col-4">
+                       <ContactForm add={this.addContact}/> 
+                    </div>
+                    <ContactList contacts={this.state.contacts}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/data/contacts.js
@@ -0,0 +1,28 @@
+
+
+export default [
+    {
+        firstName: 'Olga',
+        lastName: 'Mama',
+        phone: '097-59-23-164',
+        email: 'olga@mama.com'
+    },
+    {
+        firstName: 'Nyura',
+        lastName: 'Stasik',
+        phone: '097-59-56-678',
+        email: 'toma@sos.com'
+    },
+    {
+        firstName: 'Nina',
+        lastName: 'Los',
+        phone: '097-59-23-646',
+        email: 'olen@len.com'
+    },
+    {
+        firstName: 'Kolja',
+        lastName: 'Pes',
+        phone: '097-59-23-432',
+        email: 'kolya@mail.com'
+    }
+];
