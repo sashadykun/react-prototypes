b72909c1553798a3f85c9aa93e5f2708aafdfd08
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-11-15 16:35:21 -0800

    started

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import Welcome from './welcome';
 
-
+export default () => {
+    
+    return (
+    <div className="container">
+        <h1>This is ROUTING!</h1>
+        <Route path="/" component={Welcome}/>
+    </div>
+);
+}
---
+++ src/components/welcome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default props => (
+   <div>
+        <h1>Welcome to our site</h1>
+        <p>
+Conveying meaning to assistive technologies
+Using color to add meaning only provides a visual indication, 
+which will not be conveyed to users of assistive technologies – 
+such as screen readers. Ensure that information denoted by the color
+ is either obvious from the content itself (e.g. the visible text),
+  or is included through alternative means, such as additional text 
+  hidden with the .sr-only class.
+        </p>
+    </div>
+    )
